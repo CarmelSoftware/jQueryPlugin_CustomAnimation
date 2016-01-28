14c6b2009f7b7aa251d2ab9efacf789f7d223423
Carmel Schvartzman <CarmelSoftware@users.noreply.github.com>
2016-01-28 17:40:34 +0200

    Update PluginCustomAnimations.js

--- Scripts/PluginCustomAnimations.js
+++ Scripts/PluginCustomAnimations.js
@@ -1,1 +1,11 @@
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/// jQuery Plugin to create custom animations
+///     - support for a callback function     
+///     - support for speed settings
+/// 
+/// You can send create an animation named "customAnimation" 
+///     defined by the options : "params" 
+/// customAnimation = the name of the custom animation 
+/// params = the animation to be done , for example :  { 'opacity': 'toggle', 'height': 'toggle' }
+////////////////////////////////////////////////////////////////////////////////////////////////////
