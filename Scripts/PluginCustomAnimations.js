484a0e072c2978abf479cdd57d5f502f63b577c9
Carmel Schvartzman <CarmelSoftware@users.noreply.github.com>
2016-02-02 09:31:52 +0200

    Update PluginCustomAnimations.js

--- Scripts/PluginCustomAnimations.js
+++ Scripts/PluginCustomAnimations.js
@@ -10,6 +10,22 @@
 /// params = the animation to be done , for example :  { 'opacity': 'toggle', 'height': 'toggle' }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+_.fn.createCustomAnimation = function (customAnimation, params)
+{
+
+    var _this = _(this);
+
+    _.fn[customAnimation] = function (speed, callback)
+    {
+        return this.animate(
+            params,
+            speed,
+            _.isFunction(callback) && _.proxy(callback, this)
+            );
+    };
+
+    return _this;
+};
 
 
 
